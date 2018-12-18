f45bed46ece307e92a08b9bbbed48ed9d38f715e
Guillaume <gservais98@gmail.com>
2018-12-18 10:23:32 +0100

    select company in table + swap buttons

--- back-office/src/app/company-management/company-management.component.ts
+++ back-office/src/app/company-management/company-management.component.ts
@@ -3,6 +3,7 @@ import { TabsService } from '../tabs.service';
 import { Company } from '../model/Company';
 import { MatPaginator, MatSort } from '@angular/material';
 import { DataTableDataSource } from './data-table-datasource';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-company-management',
@@ -19,8 +20,10 @@ export class CompanyManagementComponent implements OnInit {
   updateDisabled : boolean = false;
   companiesArray : Array<Company>;
   columnsToDisplay = ['name', 'address', 'sector'];
+  selectedRowName: string = "";
+  selectedCompany : Company;
 
-  constructor() {
+  constructor(private router: Router) {
     this.tabsService = new TabsService("companyTab");
     this.companiesArray = new Array();
 
@@ -30,16 +33,36 @@ export class CompanyManagementComponent implements OnInit {
 
   ngOnInit() {
     this.tabsService.setActive();
+    this.swapButton("listCompanies");
     this.dataSource = new DataTableDataSource(this.paginator, this.sort);
     this.dataSource.addCompanies(this.companiesArray);
   }
 
-  testFunction(row){
-    console.log(row);
+  selectCompany(row){
+    this.selectedRowName = row.name;
+    this.selectedCompany = row;
+    this.updateDisabled = true;
   }
 
-  companySelected(){
-    this.updateDisabled = true;
+  swapButton(button){
+    this.removeOldActive();
+    switch(button){
+      case 'listCompanies':
+        document.getElementById(button).classList.toggle("active");
+        break;
+      case 'addCompany' : 
+        document.getElementById(button).classList.toggle("active");
+        break;
+      case 'updateCompany' : 
+        document.getElementById(button).classList.toggle("active");
+        break;
+    }
+  }
+
+  removeOldActive(){
+    let div = document.getElementById("listButtons");
+    let activeButton = div.getElementsByClassName("active")[0];
+    if(activeButton != null ) activeButton.classList.toggle("active");
   }
 
 }
